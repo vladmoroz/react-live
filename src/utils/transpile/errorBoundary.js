59471514496d6f01bdbe54ef357c7f4c7cddef10
Vlad Moroz <hi@vladmoroz.com>
2020-05-13 15:11:10 +0300

    Better error handling

--- src/utils/transpile/errorBoundary.js
+++ src/utils/transpile/errorBoundary.js
@@ -2,12 +2,62 @@ import React, { Component } from 'react';
 
 const errorBoundary = (Element, errorCallback) => {
   return class ErrorBoundary extends Component {
+    constructor() {
+      super();
+      this.checkValidity();
+    }
+
     componentDidCatch(error) {
       errorCallback(error);
     }
 
+    componentDidUpdate() {
+      this.checkValidity();
+    }
+
+    checkValidity() {
+      try {
+        const isValid =
+          this.isValidNode() ||
+          this.isClassComponent() ||
+          React.isValidElement(<Element />);
+
+        if (!isValid) {
+          throw new Error(`${Element} is not a valid React child`);
+        }
+      } catch (error) {
+        errorCallback(new Error(`${Element} is not a valid React child`));
+      }
+    }
+
+    isValidNode() {
+      return (
+        React.isValidElement(Element) ||
+        typeof Element === 'boolean' ||
+        typeof Element === 'number' ||
+        typeof Element === 'string' ||
+        Element === null
+      );
+    }
+
+    isClassComponent() {
+      return Element.prototype instanceof Component;
+    }
+
     render() {
-      return typeof Element === 'function' ? <Element /> : Element;
+      if (this.isValidNode()) {
+        return Element;
+      }
+
+      try {
+        if (this.isClassComponent() || React.isValidElement(<Element />)) {
+          return <Element />;
+        }
+      } catch (error) {
+        return null;
+      }
+
+      return null;
     }
   };
 };
